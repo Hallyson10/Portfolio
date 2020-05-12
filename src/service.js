e91df33906d23db652c6ec310b609c965ce7e942
Hallyson Miranda <hallysonjean@hotmail.com>
2020-05-12 13:07:15 -0300

    Adicionando variavel de ambiente

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : 'https://portfolio-project-x19.herokuapp.com/'
+    baseURL : process.env.REACT_APP_API_URL
 })
 export default api;
