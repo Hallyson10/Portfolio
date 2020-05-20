efde35c5e888492405b9dac3ad96c45a3d9a9fce
Hallyson Miranda <hallysonjean@hotmail.com>
2020-05-20 06:46:30 -0300

    inserindo mais posts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,6 +11,9 @@ import image1 from '../../assets/imaget1.jpg'
 import image2 from '../../assets/Imaget2.svg'
 import image3 from '../../assets/imaget3.jpg'
 import image4 from '../../assets/imaget4.jpg'
+import imagep1 from '../../assets/fotoFotogenica.jpg'
+import imagep2 from '../../assets/fotoFotogenica3.jpg'
+import imagep3 from '../../assets/fotoFotogenica23.jpg'
 
 import $ from 'jquery';
 import { getProjetos } from '../../funcoes/index'
@@ -26,7 +29,36 @@ function Home({match,history}){
       imagem_principal : image3
     },{
       imagem_principal : image4
-    }])
+    },{
+      imagem_principal : imagep1
+    },
+    {
+      imagem_principal : imagep2
+    },
+    {
+      imagem_principal : imagep3
+    },
+    {
+      imagem_principal : imagep1
+    },
+    {
+      imagem_principal : image1
+    },
+    {
+      imagem_principal : image4
+    },
+    {
+      imagem_principal : image1
+    },{
+      imagem_principal : image2
+    },{
+      imagem_principal : image3
+    },{
+      imagem_principal : image4
+    },{
+      imagem_principal : imagep1
+    },
+  ])
     const [user,setUser] = useState({descricao_pessoal : 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures'});
     
     // useEffect(()=>{
